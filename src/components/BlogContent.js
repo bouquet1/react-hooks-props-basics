100fb37393bc94b4da99ff261c975b214550607d
bouquet1 <bukuleta@gmail.com>
2023-05-26 17:12:15 -0700

    discovering props

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -5,3 +5,27 @@ function BlogContent(props) {
 }
 
 export default BlogContent;
+
+/*
+Having the ability to share this data between components by passing props down to a child component from a parent makes our components incredibly flexible! For example, here's how we could expand our BlogContent component based on those additional props:
+
+function BlogContent(props) {
+  console.log(props);
+
+  if (!props.isPublished) {
+    // hide unpublished content
+    // return null means "don't display any DOM elements here"
+    return null;
+  } else {
+    // show published content
+    return (
+      <div>
+        <h1>{props.articleText}</h1>
+        <p>{props.minutesToRead} minutes to read</p>
+      </div>
+    );
+  }
+}
+
+Above, we are using conditional renderingLinks to an external site. to only display the blog content if it is published, based on the isPublished prop.
+*/
--- src/components/Comment.js
+++ src/components/Comment.js
@@ -5,3 +5,20 @@ function Comment(props) {
 }
 
 export default Comment;
+
+/*
+This component, when used, will display content that is passed down to it, allowing us to pass different content to multiple Comment components. Let's add them in. Of course, with components being re-usable, we can make as many as we want:
+
+This component, when used, will display content that is passed down to it, allowing us to pass different content to multiple Comment components. Let's add them in. Of course, with components being re-usable, we can make as many as we want. ...and just as before, we can pass content data down to them:
+
+function BlogPost() {
+  return (
+    <div>
+      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+      <Comment commentText="I agree with this statement. - Angela Merkel" />
+      <Comment commentText="A universal truth. - Noam Chomsky" />
+      <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+    </div>
+  );
+}
+*/
